51e78ebca408676400293c26545a83413a72963c
armstrxng <mikellaarmstrong@gmail.com>
2015-03-09 17:40:00 -0500

    Added some Asychronous actions in the fragments

--- app/src/main/java/com/csis290/learnfragments/MainActivity.java
+++ app/src/main/java/com/csis290/learnfragments/MainActivity.java
@@ -1,9 +1,17 @@
 package com.csis290.learnfragments;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+
+import java.util.logging.LogRecord;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +19,24 @@ public class MainActivity extends Activity {
     private TextView tvGreen;
     private TextView tvBlue;
     private TextView tvRed;
+
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            Bundle bundle = new Bundle();
+            bundle.putString("key", "Welcome to the Activity!");
+
+            GreenFragment greenFragment = new GreenFragment();
+            greenFragment.setArguments(bundle);
+
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, greenFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .addToBackStack("")
+                    .commit();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +50,14 @@ public class MainActivity extends Activity {
         tvGreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString("key", "Welcome to the Activity!");
-
-                GreenFragment greenFragment = new GreenFragment();
-                greenFragment.setArguments(bundle);
-
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, greenFragment)
-                        .addToBackStack("")
-                        .commit();
+                Handler handler = new Handler();
+                handler.postDelayed(r, 1500);
             }
         });
         tvBlue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString("key", "Welcome to the Activity");
-
-                BlueFragment blueFragment = new BlueFragment();
-                blueFragment.setArguments(bundle);
-
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, blueFragment)
-                        .addToBackStack("")
-                        .commit();
+               new BlueTransactionTask(new Handler()).execute();
             }
         });
 
@@ -57,9 +66,59 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 getFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new RedFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                         .addToBackStack("")
                         .commit();
             }
         });
     }
+    // AsycnTask<Params, Progress, Result>
+class BlueTransactionTask extends AsyncTask<Void, Void, Void>
+    {
+        private Handler handler;
+
+        Runnable blueRunnable = new Runnable() {
+            @Override
+            public void run()
+            {
+                Bundle bundle = new Bundle();
+                bundle.putString("key", "Welcome to the Activity");
+
+                BlueFragment blueFragment = new BlueFragment();
+                blueFragment.setArguments(bundle);
+
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, blueFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .addToBackStack("")
+                        .commit();
+            }
+        };
+
+        public BlueTransactionTask(Handler handler)
+        {
+            this.handler = handler;
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            Log.d("tag", "onPreExecute()");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            handler.postDelayed(blueRunnable, 2000);
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            Log.d("tag", "postExecute()");
+            super.onPostExecute(aVoid);
+        }
+    }
 }
+
